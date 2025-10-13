035833becf6f43e39b76064aa711367b2c7e479e
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-10-13 21:47:12 +0000

    removed preface comment

--- src/main.cu
+++ src/main.cu
@@ -1,24 +1,3 @@
-/****************************************************************************
- *
- * cuda-hello0.cu - Hello world with CUDA (no device code)
- *
- * Based on the examples from in the CUDA toolkit documentation
- * http://docs.nvidia.com/cuda/cuda-c-programming-guide/
- *
- * Last updated in 2017 by Moreno Marzolla <https://www.moreno.marzolla.name/>
- *
- * ---------------------------------------------------------------------------
- *
- * Compile with:
- *
- *      nvcc cuda-hello0.cu -o cuda-hello0
- *
- * Run with:
- *
- *      ./cuda-hello0
- *
- ****************************************************************************/
-
 #include <stdint.h>
 #include <stdio.h>
 
