cdd232202da8529237ced146a1cc1f95ca1fbe5d
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-10-13 21:44:24 +0000

    cuda working

--- src/main.cu
+++ src/main.cu
@@ -22,15 +22,23 @@
 #include <stdint.h>
 #include <stdio.h>
 
-namespace {
 __global__ void ker() {
   const uint32_t id = threadIdx.x;
   printf("Thread ID: %u\n", id);
 }
-}  // namespace
 
 int main(void) {
   printf("Hello, world!\n");
   ker<<<1, 1>>>();
+
+  const cudaError_t err = cudaDeviceSynchronize();
+  if (err == cudaSuccess) {
+    printf("CUDA: success\n");
+  } else {
+    const int32_t _ =
+        fprintf(stderr, "CUDA: error: %s\n", cudaGetErrorString(err));
+    return 1;
+  }
+
   return 0;
 }
